3c279ecc1c9591c63059c765e79e8b7f5991b655
steelydylan <ess_president@me.com>
2017-03-17 23:44:51 +0900

    added editorconfig

--- test/src/index.jsx
+++ test/src/index.jsx
@@ -3,55 +3,55 @@ import ReactDOM from 'react-dom'
 import ReactAtable from '../../lib/index.js'
 
 const style = {
-	float:"left",
-	width:"50%",
-	height:"500px",
-	boxSizing:"border-box"
+  float: "left",
+  width: "50%",
+  height: "500px",
+  boxSizing: "border-box"
 }
 
 const tableStyle = {
-	float:"left",
-	width:"50%",
-	boxSizing:"border-box",
-	backgroundColor:"#666",
-	padding:"10px"
+  float: "left",
+  width: "50%",
+  boxSizing: "border-box",
+  backgroundColor: "#666",
+  padding: "10px"
 }
 
 class App extends React.Component {
 
-  constructor () {
+  constructor() {
     super();
-		this.state = {
-			val:""
-		};
+    this.state = {
+      val: ""
+    };
   }
 
-  render () {
+  render() {
     return (
-			<div>
-				<div style={tableStyle}>
-					<ReactAtable onUpdate={(html)=>this.setState({val:html})}>
-						<table>
-							<tbody>
-								<tr>
-									<th>test</th>
-									<th>test</th>
-								</tr>
-								<tr>
-									<td>hoge</td>
-									<td>hoge</td>
-								</tr>
-							</tbody>
-						</table>
-					</ReactAtable>
-				</div>
-				<textarea value={this.state.val} style={style}></textarea>
-			</div>
+      <div>
+        <div style={tableStyle}>
+          <ReactAtable onUpdate={(html) => this.setState({ val: html })}>
+            <table>
+              <tbody>
+                <tr>
+                  <th>test</th>
+                  <th>test</th>
+                </tr>
+                <tr>
+                  <td>hoge</td>
+                  <td>hoge</td>
+                </tr>
+              </tbody>
+            </table>
+          </ReactAtable>
+        </div>
+        <textarea value={this.state.val} style={style}></textarea>
+      </div>
     )
   }
 }
 
 ReactDOM.render(
   <App />,
-    document.getElementById('root')
+  document.getElementById('root')
 )
